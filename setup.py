0a5acceec2f665cf8b221f8704ea67c078e16a2e
Anas El Mhamdi <anas.elmhamdi@gmail.com>
2024-10-25 21:17:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="ssb-snowflake-stage-exporter",
+    name="ssb-scrapy-snowflake-stage-exporter",
     version="0.0.1",
     description="Snowflake database exporting utility with Scrapy integration",
     packages=find_packages(),
